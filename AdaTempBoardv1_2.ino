29527115421d07bfb1d779586c16993c678b4623
JorgeNeusch <142831073+JorgeNeusch@users.noreply.github.com>
2024-09-20 12:09:37 -0500

    Add files via upload

---
+++ AdaTempBoardv1_2.ino
@@ -0,0 +1,180 @@
+
+//Include Wire I2C library,SPi library, & Adafruit CAN
+#include <Wire.h>
+#include <SPI.h>
+#include <CANController.h>
+#include <CANSAME5x.h>
+#include <CANSAME5x_port.h>
+
+#define PIN_CAN_STANDBY 1
+#define PIN_CAN_BOOSTEN 2
+
+//Baudrate is set 
+#define Baudrate 9600
+
+
+//Static addresses for ADC I2C
+#define ADC1 0
+#define BytesRequest 2 //Request 2 bytes from the ADC
+#define ADCBitSize 12 //predetermined setting for amount of bits sent
+#define MaxBitSize 16 // Max size of the ADC
+
+#define LSBVolt .001  //the voltage of LSB for 12bit
+#define PGA 1 //gain on resolution of ADC
+
+//SYNC pinout
+#define SYNC 13
+
+
+int16_t VtoTforF24(uint16_t x)//
+{
+  int I =-474.75*pow(x,6)+4616.6*pow(x,5)-17903*pow(x,4)+34634*pow(x,3)-33495*pow(x,2)+13014*x;
+  return I;
+}
+
+void setup()
+{
+    //Start serial communication at some baud
+  Serial.begin(Baudrate);
+  //       SPI Set up
+  pinMode(SYNC,OUTPUT);//sets the MUX sync bit to pin 13
+  digitalWrite(SYNC,HIGH);//sets to high to intialize SPI
+  // CAN Setup
+  pinMode(PIN_CAN_STANDBY,OUTPUT);//CAN Standby 
+  digitalWrite(PIN_CAN_STANDBY,false); // turn off STANDBY
+  pinMode(PIN_CAN_BOOSTEN,OUTPUT);
+  digitalWrite(PIN_CAN_BOOSTEN,true); // turn on booster
+
+  uint8_t config = 144; //set to default config on boot-up, but it's changeable
+
+  //configuration byte for the ADC
+/*
+Consists of bits RDY,C1,C2,O/C,S1,S0,G1,G0
+
+RDY - in writing intiates a new conversation with 1, but has no affect in in continous conversion
+
+C1-C0 - Channel select, so should be left 00
+
+0/C - Channel Mode; 1 is Continous Conversion mode, 0 is One-shot Conversion mode
+
+S1-S0 - Sample Rate Selection Bit 
+        00 = 240 SPS (12 bits)
+        01 = 60 SPS (14 bits)
+        10 = 15 SPS (16 bits)
+
+G1-G0 - PGA Gain Selector Bits
+        00 = 1 V/V
+        01 = 2 V/V
+        10 = 4 V/V
+        11 = 8 V/V
+
+Default config is 10010000, or 144 meaning continous conversion, at 12 bits, and 1 V/V gain
+
+*/
+
+  Wire.begin(); //intializes I2C
+    delay(1);
+
+//writes to the ADC and sends the config bit
+  Wire.beginTransmission(ADC1);
+  Wire.write(config);
+  Wire.endTransmission();
+
+    
+ SPI.begin();
+      delay(1);
+
+}
+
+void loop() {
+uint8_t AMux = 0; //intializes the address of the Mux at 0
+
+  while(1);
+{
+  delay(1);
+  
+  digitalWrite(SYNC,LOW);// sets to low to send the ack bit to MUX
+
+  //begin SPI request to Mux
+  SPI.beginTransaction(SPISettings(30000,MSBFIRST,SPI_MODE1));
+  
+  /*
+  SPI Modes
+  0 - Clock Polarity is 0, data cap on Rising
+  1- Clock Polarity is 0, data cap on Falling
+  2- Clock Polarity is 1, data cap on Falling
+  3- Clock Polarity is 1, data cap on Rising
+  
+  We want spi mode 1 or 2 to cap falling edge, Max Hz is 30,000
+  
+  */
+  SPI.transfer(AMux);
+  
+   /*transfers the address byte for MUX
+
+  Consits of bits in MSB EN,CSA,CSB,X,A3,A2,A1,A0
+  
+  EN - when 1, all switches off
+
+  CSA/CSB - when both 1, retains the previuos Switch Condition
+
+  A0-A3 - Address of Switch begining with 0000 where S1A -> DA, S1B ->DB
+          ending with the 1111 where S16A -> DA, S16B -> DB
+
+    For our purpuse, we will start with 0 and increase to a max of 15 to cycle
+    all the addresses.
+
+  */
+  SPI.endTransaction(); //end SPI
+   digitalWrite(SYNC,HIGH); // indicates end of SYNC
+
+  if(AMux == 15){ // cycles the addresses of SPI
+    AMux = 0;
+  }
+  else{
+     AMux++;
+  }
+
+ if(!Wire.available()){ //check to see if I2C connects
+    Serial.print("I2C failed to conect to ADC");
+  }
+  //Request 2 bytes from the specified address
+  Wire.requestFrom(ADC1,BytesRequest); 
+  int16_t remaining = BytesRequest;
+  uint16_t package = 0;
+
+  while(Wire.available() && --remaining){
+  // Get the data in 2 byetes and assign to x and y
+    uint8_t x =  Wire.read();
+    uint8_t y =  Wire.read();
+    package = (y << 8) + x; //combine x and y to get full 16 bit package
+  }
+/*
+ When in 12-bit mode or 14-bit mode, the ADC will repeat the most sigificant bit after the 12-bit or 14 bit transmission.
+ To get rid of those unwanted 1s, a for loop is used to clear those bits.
+*/
+uint16_t mask = 1 << (ADCBitSize);// create a mask that is the size of the setted ADC size
+  for (uint16_t i = 0; i == MaxBitSize - ADCBitSize ;i++) // intialize i with 0 then goes till 4
+  {
+    package &= ~(mask);
+    mask = mask<<1; //mask gets rid of a leading 1
+  }
+uint16_t volt = package*LSBVolt/PGA;// formula to convert the package to volts
+
+   uint8_t cel = VtoTforF24(volt);
+
+/*
+  //Send the temperature in degrees C and F to the serial monitor
+  Serial.print(c);
+  Serial.print("C ");
+ */
+/*
+Code needs to transmit this into a CAN frames
+Frame - 1: Contains the Address Claim at 200 ms 
+
+Frame - 2: Contains the Thermistor for 
+
+*/
+  }
+}
+
